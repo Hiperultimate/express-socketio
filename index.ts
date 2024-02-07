684fd5eca91fb9524ba86d71d234fae775164943
Hiperultimate <hiperultimatevegito@gmail.com>
2024-02-07 17:02:31 +0530

    Made a socket io server

--- index.ts
+++ index.ts
@@ -1,16 +1,40 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import cors from "cors";
 import express, { Express, Request, Response } from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
-dotenv.config()
+dotenv.config();
 
 const app: Express = express();
-const port = 3000;
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/serverstatus", (req: Request, res: Response) => {
   console.log("Check server status");
-  res.json({status: "OK"});
+  res.json({ status: "OK" });
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+    // socket.broadcast.emit(msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`App listening on port http://localhost:${process.env.PORT}`);
 });
